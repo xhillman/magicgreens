0cc692d7748fea8b476a64f22327b616821f5efd
Xavier Hillman <xhillman13@gmail.com>
2023-01-05 17:07:11 -0500

    v1 homepage styling

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -12,13 +12,19 @@ function Home() {
         <h2>Overlay text will go here.</h2>
         <button>Shop Now</button>
       </div>
-      <h3 className='tagline'>Fast, Fresh, Thoughtful</h3>
-      <img className='hero-image' src="https://via.placeholder.com/300x200" />
-      <p>Fast Paragraph - Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti fuga animi, accusamus ipsam iste exercitationem dolorem atque sint, expedita modi et, error suscipit. Soluta id delectus dolorem laudantium illum veniam.</p>
-      <img src="https://via.placeholder.com/300x200" />
-      <p>Fresh Paragraph - Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti fuga animi, accusamus ipsam iste exercitationem dolorem atque sint, expedita modi et, error suscipit. Soluta id delectus dolorem laudantium illum veniam.</p>
-      <img src="https://via.placeholder.com/300x200" />
-      <p>Thoughtful Paragraph - Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti fuga animi, accusamus ipsam iste exercitationem dolorem atque sint, expedita modi et, error suscipit. Soluta id delectus dolorem laudantium illum veniam.</p>
+      <h3 className='tagline'>Fast - Fresh - Thoughtful</h3>
+      <div className='value-section'>
+        <img className='value-image' src="https://via.placeholder.com/300x200" />
+        <p className="value-paragraph">Fast Paragraph - Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti fuga animi, accusamus ipsam iste exercitationem</p>
+      </div>
+      <div className='value-section'>
+        <p className="value-paragraph">Fresh Paragraph - Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti fuga animi, accusamus ipsam iste exercitationem</p>
+        <img className='value-image' src="https://via.placeholder.com/300x200" />
+      </div>
+      <div className='value-section'>
+        <img className='value-image' src="https://via.placeholder.com/300x200" />
+        <p className="value-paragraph">Thoughtful Paragraph - Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti fuga animi, accusamus ipsam iste exercitationem</p>
+      </div>
     </>
   )
 }
