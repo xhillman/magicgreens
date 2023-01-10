cc10dfdf5dd7b484a2d9ca16897b6a3e81c76f23
Xavier Hillman <xhillman13@gmail.com>
2023-01-09 19:51:47 -0500

    layout and style product cards

--- src/components/ProductCard/index.tsx
+++ src/components/ProductCard/index.tsx
@@ -7,7 +7,7 @@ interface ProductCardProps {
 
 function ProductCard(props: ProductCardProps) {
 
-  const {name, price} = props;
+  const { name, price } = props;
 
   const formatter = new Intl.NumberFormat('en-US', {
     style: 'currency',
@@ -17,8 +17,11 @@ function ProductCard(props: ProductCardProps) {
   return (
     <div className="product-card">
       <img src=""></img>
-      <h3>{name}</h3>
-      <p>{formatter.format(price)}</p>
+      <div className='product-info'>
+        <h3>{name}</h3>
+        <p>{formatter.format(price)}</p>
+        <button className='add-to-cart-button'>Add to Cart</button>
+      </div>
     </div>
   )
 }
--- src/components/Products/index.tsx
+++ src/components/Products/index.tsx
@@ -1,5 +1,5 @@
 import Header from "../Header";
-import {useAppSelector} from '../../Store/hooks';
+import { useAppSelector } from '../../Store/hooks';
 import { Product } from "../../Store/slices/productsSlice";
 import ProductCard from "../ProductCard";
 
@@ -11,24 +11,27 @@ function Products() {
   console.log(products)
 
   return (
-    <div className="product-section">
+    <>
       <Header />
-      <h1>Products</h1>
-      <div className="products">
-
-      {
-        products.products.map((product: Product) => {
-          return (
-            <ProductCard name={product.name} price={product.price} />
-            // <div className="product-card" key={product.name}>
-            //   <h3>{product.name}</h3>
-            //   <p>{product.price}</p>
-            // </div>
-          )
-        })
-      }
+      <div className="product-section">
+        <h1>The Goods</h1>
+        <div className="product-container">
+          <div className="products">
+            {
+              products.products.map((product: Product) => {
+                return (
+                  <ProductCard name={product.name} price={product.price} />
+                  // <div className="product-card" key={product.name}>
+                  //   <h3>{product.name}</h3>
+                  //   <p>{product.price}</p>
+                  // </div>
+                )
+              })
+            }
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
