449d7894d46d64a6af6f035963b9d8b1abed6c58
Xavier Hillman <xhillman13@gmail.com>
2023-01-10 22:31:02 -0500

    add dynamic change to cart

--- src/components/Cart/index.tsx
+++ src/components/Cart/index.tsx
@@ -1,28 +1,38 @@
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 import { CartItem } from '../../Store/slices/cartSlice';
+import { useAppSelector } from '../../Store/hooks';
 
 import './cart.css';
 
 function Cart() {
+
+  const stateCartItems = useAppSelector((state) => state.cart);
 
   let cartItemsObject = localStorage.getItem('cart');
   if(typeof cartItemsObject !== 'string') {
     cartItemsObject = '[]';
   }
   let cartItems: CartItem[] = JSON.parse(cartItemsObject);
-  
-  console.log('cartItems', typeof cartItems, cartItems)
+
+  const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  });
+
+  useEffect(() => {
+  }, [stateCartItems])
 
   return (
     <div id='cart'>
-      <h1>Cart</h1>
+      <h1 className='cart-header'>Shopping Cart</h1>
       {
         cartItems.map((item: any) => {
           return (
             <div className='cart-item'>
               <h3>{item.name}</h3>
-              <p>{item.price}</p>
-              <p>{item.quantity}</p>
+              <p>{formatter.format(item.price)}</p>
+              <p>Quantity: {item.quantity}</p>
+              <button className='remove-from-cart'>Delete</button>
             </div>
           )
         })
