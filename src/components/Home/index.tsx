26a4aa62eb0fa1d1a8ddcf0e020540b90aa75f55
Xavier Hillman <xhillman13@gmail.com>
2023-01-17 16:25:53 -0500

    add content to home page

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,45 +1,48 @@
 import Header from "../Header";
 
-import heroLeaf from '../../../public/assets/hero-leaf.jpg';
-import fresh from '../../../public/assets/fresh-image1.png';
-import fast from '../../../public/assets/fast-image.png';
+import fresh from '../../../public/assets/fresh-image.mp4';
+import fast from '../../../public/assets/delivery-image.png';
+import thoughtful from '../../../public/assets/thoughtful-image.png';
 import './home.css';
 
 function Home() {
   return (
     <>
-    <Header />
-    <div className='home-container'>
-      <div className='hero-section'>
-        <div className="hero-container">
-          <div className="headings">
-            <h2 className="hero-heading">MagicGreens</h2>
-            <p className="hero-subheading">Fast - Fresh - Thoughtful</p>
+      <Header />
+      <div className='home-container'>
+        <div className='hero-section'>
+          <div className="hero-container">
+            <div className="headings">
+              <h2 className="hero-heading">MagicGreens</h2>
+              <p className="hero-subheading">Fast - Fresh - Thoughtful</p>
+            </div>
+            <a href='thegoods'><button className="shop-now-button">Shop Now</button></a>
           </div>
-          <a href='thegoods'><button className="shop-now-button">Shop Now</button></a>
+        </div>
+        <div className='feature-section'>
+          <div className='value-section'>
+            <img className='value-image' src={fast} />
+            <div className="hero-content-container">
+              <p className="value-paragraph">We're a part of your community, so food get to your plate faster.</p>
+            </div>
+          </div>
+          <div className='value-section'>
+            <div className="hero-content-container">
+              <p className="value-paragraph">Controlled growing environments mean fresh food all year long.</p>
+            </div>
+            <video className='value-video' autoPlay loop muted>
+              <source src={fresh} type="video/mp4" />
+            </video>
+            {/* <img className='value-image' src={fresh} /> */}
+          </div>
+          <div className='value-section'>
+            <img className='value-image' src={thoughtful} />
+            <div className="hero-content-container">
+              <p className="value-paragraph">97% less water<br/>11x more yield/acre<br/>ZERO pesticides </p>
+            </div>
+          </div>
         </div>
       </div>
-      <div className='feature-section'>
-      <div className='value-section'>
-        <img className='value-image' src={fast} />
-        <div className="hero-content-container">
-          <p className="value-paragraph">We're a part of your community, so foods get to your plate faster.</p>
-        </div>
-      </div>
-      <div className='value-section'>
-        <div className="hero-content-container">
-          <p className="value-paragraph">Controlled growing environments mean fresh food all year long.</p>
-        </div>
-        <img className='value-image' src={fresh} />
-      </div>
-      <div className='value-section'>
-        <img className='value-image' src={fresh} />
-        <div className="hero-content-container">
-          <p className="value-paragraph">Thoughtful Paragraph</p>
-        </div>
-      </div>
-      </div>
-    </div>
     </>
   )
 }
