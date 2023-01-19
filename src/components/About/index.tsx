7a7b3b7960edded2d2a6ca53d87f7f3d4c20b920
Xavier Hillman <xhillman13@gmail.com>
2023-01-19 17:16:51 -0500

    add animation to about timeline items

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,7 +1,49 @@
 import Header from "../Header";
+import {useRef, useEffect} from 'react';
 import './about.css';
 
 function About() {
+
+  const leftRef = useRef<HTMLDivElement[]>([]);
+  const rightRef = useRef<HTMLDivElement[]>([]);
+  const pushLeftRef = (el: HTMLDivElement) => leftRef.current.push(el);
+  const pushRightRef = (el: HTMLDivElement) => rightRef.current.push(el);
+
+  useEffect(() => {
+
+    let options = {
+      root: null,
+      rootMargin: '0px',
+      threshold: .75
+    }
+
+    const leftObserver = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('show-left');
+          console.log(entries);
+        } else {
+          entry.target.classList.remove('show-left');
+        }
+      })
+    }, options);
+
+    const rightObserver = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('show-right');
+          // console.log(entries);
+        } else {
+          entry.target.classList.remove('show-right');
+        }
+      })
+    }, options);
+
+    leftRef.current.forEach(el => leftObserver.observe(el));
+    rightRef.current.forEach(el => rightObserver.observe(el));
+    
+  }, [])
+
   return (
     <>
       <Header />
@@ -9,27 +51,27 @@ function About() {
         <h1>Our Story</h1>
         <div className="about-timeline">
           <div className='timeline'></div>
-          <div className="timepoint-left">
+          <div ref={pushLeftRef} className='hidden-left'>
             <h2>2017</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum dolor minus commodi.</p>
           </div>
-          <div className="timepoint-right">
+          <div ref={pushRightRef} className='hidden-right'>
             <h2>2018</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum dolor minus commodi.</p>
           </div>
-          <div className="timepoint-left">
+          <div ref={pushLeftRef} className='hidden-left'>
             <h2>2019</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum dolor minus commodi.</p>
           </div>
-          <div className="timepoint-right">
+          <div ref={pushRightRef} className='hidden-right'>
             <h2>2020</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum dolor minus commodi.</p>
           </div>
-          <div className="timepoint-left">
+          <div ref={pushLeftRef} className='hidden-left'>
             <h2>2021</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum dolor minus commodi.</p>
           </div>
-          <div className="timepoint-right">
+          <div ref={pushRightRef} className='hidden-right'>
             <h2>2022</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum dolor minus commodi.</p>
           </div>
